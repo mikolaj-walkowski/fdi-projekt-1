18c13413d25e5e318593dea86f00fb7080ab1933
Krzysztof Jajeśnica <krzysztof.jajesnica@gmail.com>
2020-07-30 12:41:01 +0200

    Wstępne GUI
    - GUI do ustawiania parametrów & uruchamiania symulacji
    - Można już wyświetlać polskie i greckie znaki! (chwilowo działa tylko na Windowsie)
    - Automatyczne konwersje między wektorami z GLMa oraz ImGui

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
-#include <cassert>
 #include <iostream>
 #include <exception>
 
+#include <glm/glm.hpp>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
@@ -9,6 +9,8 @@
 #include <GLFW/glfw3.h>
 
 #include <evil_macros.hpp>
+
+const auto YELLOW = glm::vec4(1,1,0,1);
 
 void errorCallback(int error, const char* description)
 {
@@ -51,8 +53,6 @@ int main(int, char**)
     // Inicjalizacja ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    ImGui::StyleColorsClassic();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
     onExit(
@@ -60,18 +60,67 @@ int main(int, char**)
         ImGui_ImplGlfw_Shutdown();
         ImGui::DestroyContext();
     );
+ 
+    // Musimy załadować czcionkę do polskich znaków, domyślnie ImGui ładuje tylko ASCII
+    ImGuiIO& io = ImGui::GetIO(); 
+    auto *fonts = io.Fonts;
+    ImWchar16 unicodeGlyphRanges[] = {
+        0x20, 0xFF,   //ASCII
+        0x100, 0x17F, //Latin Extended-A (polskie znaki)
+        0x370, 0x3FF, //Greka
+        0
+    };
+    #if _WIN32
+        fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Arial.ttf", 16, NULL, unicodeGlyphRanges);
+    #endif
 
-    ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    bool showDemo = true;
+    auto clearColor = glm::vec4(0,0,0.2,1);
+    auto containerDimensions = glm::vec2(40, 40);
+    int maxNumParticles = static_cast<int>(containerDimensions.x * containerDimensions.y),
+        numParticles = maxNumParticles/2,
+        detectorDelay = 100;
+    float collisionTollerance = 0.05f,
+          maxInitialVelocity = 5,
+          detectorY = 20, 
+          detectorHeight = 5,
+          dt = 1;
 
     while (!glfwWindowShouldClose(window))
     {
-
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::ShowDemoWindow(&showDemo);
+        
+        ImGui::Begin("Ustawienia symulacji");
+            if(ImGui::CollapsingHeader("Cząstki")) 
+            {
+                ImGui::TextColored(YELLOW, "Stan początkowy");
+                ImGui::SliderInt("Liczba", &numParticles, 1, maxNumParticles, "n = %d");
+                ImGui::SliderFloat("Max. prędkość", &maxInitialVelocity, 0, 100, "V = %.2f");
+
+                ImGui::TextColored(YELLOW, "Zderzenia");
+                ImGui::SliderFloat("Tolerancja", &collisionTollerance, 0, 0.1f, "d = %.2fR");
+            }
+            if(ImGui::CollapsingHeader("Zbiornik"))
+            {
+                ImGui::TextColored(YELLOW, "Wymiary zbiornika");
+                ImGui::SliderFloat("Szerokość", &containerDimensions.x, 20, 100, "L = %.2fR");
+                ImGui::SliderFloat("Wysokość", &containerDimensions.y, 20, 100, "H = %.2fR");
+                maxNumParticles = static_cast<int>(containerDimensions.x * containerDimensions.y);
+
+                ImGui::TextColored(YELLOW, "Detektor");
+                ImGui::SliderFloat("Położenie", &detectorY, 0, containerDimensions.y, "h = %.2fR");
+                ImGui::SliderFloat("Wysokość ", &detectorHeight, 0, 20, "l = %.2fR");
+                ImGui::SliderInt("Opóźnienie", &detectorDelay, 0, 500, "T = %dδt");
+            }
+            ImGui::Separator();
+            ImGui::SliderFloat("Krok symulacji", &dt, 0.05f, 2, "δt = %.3f");
+
+            if(ImGui::Button("ROZPOCZNIJ SYMULACJĘ", ImVec2(ImGui::GetWindowContentRegionWidth(), 0)))
+                std::cerr << "Jeszcze nie zaimplementowano.";
+
+        ImGui::End();
 
         ImGui::Render();
         glViewport(0, 0, fboWidth, fboHeight);
